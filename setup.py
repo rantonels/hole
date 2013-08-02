07141fbe03978a7f645fe719cc694856c5992e3c
Riccardo Antonelli <riccardo.antonelli@hotmail.it>
2013-08-02 19:26:22 +0200

    created setup.py file for py2exe

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    console=["hole"]
+    options = {
+        "py2exe":{
+            "dist_dir":""
+            "unbuffered":True,
+            "excludes":["curses"],
+            "optimize":0,
+            "bundle_files":1
+        }
+    }
+    )
+
